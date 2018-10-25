20be1f0f8baec05ef87109bfefbe70d3f0f1735f
William Holcombe <sphinxelite@Williams-iMac.local>
2018-10-25 17:37:00 -0400

    removed indexjs

---
+++ client/main.js
@@ -0,0 +1,26 @@
+// import {Template} from "meteor/blaze-html-templates";
+
+// Template.NewRecipe.events({
+//     'submit .new-recipe'(event) {
+//       // Prevent default browser form submit
+//       event.preventDefault();
+   
+//       // Get value from form element
+//       const target = event.target;
+//       const name = target.name.value;
+//       const description = target.description.value;
+//    console.log(name, target.name);
+//       // Insert a task into the collection
+//       Recipes.insert({
+//         name,
+//       desc: description,
+
+//       });
+   
+//       // Clear form
+//       target.description.value = '';
+//       target.name.value = '';
+//     },
+//   });
+
+  // {{> quickForm collection="Recipes" id="insertRecipeForm" type="insert" class="new-recipe-form"}}
---
+++ collections/recipes.js
@@ -0,0 +1,43 @@
+import SimpleSchema from 'simpl-schema';
+SimpleSchema.extendOptions(['autoform']);
+
+Recipes = new Meteor.Collection('recipes');
+
+
+
+RecipeSchema = new SimpleSchema({
+    name: {
+        type: String,
+        label: "Name"
+    },
+    desc: {
+        type: String,
+        label: "Description"
+    },
+    author: {
+        type: String,
+        label: "Author",
+        autoValue: function() {
+            return this.userId
+        },
+
+        autoform: {
+            type: "hidden"
+        }
+    }, createdAt: {
+        type: Date,
+        label: "Created At",
+        autoValue: function() {
+            return new Date()
+        },
+        autoform: {
+            type: "hidden"
+        }
+    }
+});
+
+Recipes.attachSchema( RecipeSchema );
+
+//uses Aldeed +
+
+//quickform is from autoform
